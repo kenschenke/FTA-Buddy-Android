db2b4615541a55a6a31f4de412562dec07fcf134
Ken Schenke <kenschenke@gmail.com>
2025-07-13 22:10:47 -0500

    Hide system UI

--- app/src/main/java/com/kenschenke/ftabuddy/MainActivity.java
+++ app/src/main/java/com/kenschenke/ftabuddy/MainActivity.java
@@ -1,6 +1,8 @@
 package com.kenschenke.ftabuddy;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -8,7 +10,6 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,9 +29,32 @@ public class MainActivity extends AppCompatActivity {
 //        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
+
+
+        final Handler handler = new Handler();
+        final int delay = 5000;
+
+        hideSystemUI();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                hideSystemUI();
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.hide();
         }
     }
+
 }
